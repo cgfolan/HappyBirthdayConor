7a95990524a7da8ae47ec5eebc4711ad429a5d57
cgfolan <77582081+cgfolan@users.noreply.github.com>
2023-03-11 22:25:52 +0000

    Add files via upload

---
+++ Round 1-Copy1.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+score = 0
+complete = 0
+wrong= ('''
+                Try again!''')
+
+print('Welcome to round 1')
+
+#+++++1
+while score == 0:
+
+    ans = input('''1. Shania Twain recently replaced refrences to Brad Pitt with which other actor in live performances of 'That Don't Impress Me Much'?''')
+    if ans == 'Ryan Reynolds':
+                score += 1
+    else: print(wrong)
+                
+while score == 1:
+    ans = input('''2. How many Dharma Stations were there on/off the coast of the Island?''')
+    if ans == '6' or ans == 'six' or ans == 'Six':
+                score +=1
+                
+    else: print(wrong)
+        
+while score == 2:
+    ans = input('''3. What was the number 1 single in Ireland on March 12 1983? ''')
+    if ans == 'Billie-Jean' or ans == 'Billie Jean' or ans == 'Billy Jean' or ans == 'Billy-Jean' :
+                score += 1
+    else: print(wrong)    
+        
+while score == 3:
+    ans = input('''4. Who's biography is titled The Man in the Jacket?''')
+    if ans == 'Mr. Tayto' or ans == 'Mr Tayto' or ans == 'Mister Tayto':
+                score += 1
+    else: print(wrong)
+        
+while score == 4:
+    ans = input('''5. What is the collective noun for a group of ferrets?''')
+    if ans == 'business' or ans == 'a business' or ans == 'Business' or ans == 'A business' or ans == 'A Business':
+                score +=1
+    else:print(wrong)
+        
+while score == 5:
+    ans = input('''6. Which European country has the most timezones?''')
+    if ans == 'France':
+                score += 1
+    else: print(wrong)
+        
+while score == 6:
+    ans = input('''7. What element has the atomic number 40?''')
+    if ans == 'Zirconium' or ans == 'zirconium':
+                score += 1
+    else: print(wrong)
+        
+while score == 7:
+    ans = input('''8. What was Colonel Campbell's CODEC frequency in MGS 1?''')
+    if ans == '140.85':
+                score += 1
+    else: print(wrong)
+        
+while score == 8:
+    ans = input('''9. What state is Greendale Community College in? ''')
+    if ans == 'Colorado':
+                score += 1
+    else: print(wrong)
+        
+while score == 9:
+    ans = input('''10. What is the only country that begins with a Y?''')
+    if ans == 'Yemen' or ans == 'yemen':
+                score += 1
+            
+while score == 10:
+    os.system('cls')
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
